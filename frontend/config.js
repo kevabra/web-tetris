d7e56d0da1c9953d77c3c8dd15abcb847c8b89fe
Kev Abra <kevabra3570@gmail.com>
2024-09-12 22:33:00 -0400

    fix backend url

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://guarded-wave-29929-2ea77f4290be.herokuapp.com/'; // Update this to backend server url
+const BASE_URL = 'https://guarded-wave-29929-2ea77f4290be.herokuapp.com'; // Update this to backend server url
 
 export default BASE_URL;
