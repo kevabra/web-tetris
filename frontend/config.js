65d136bbe98f93a3b9cb9f02bc1467b5a0c74bf7
Kev Abra <kevabra3570@gmail.com>
2024-09-12 22:29:08 -0400

    update backend url

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://localhost:5000'; // Update this if backend runs on a different URL
+const BASE_URL = 'https://guarded-wave-29929-2ea77f4290be.herokuapp.com/'; // Update this to backend server url
 
 export default BASE_URL;
